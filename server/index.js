11f37d192e711343ec5449f965d2b37116feb16c
Armando Pinales <armandop4597@gmail.com>
2021-11-29 15:33:38 -0700

    updated page html

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ mongoose.connect(keys.mongoURI)
 // query mongodb, if valid entry is found: return that data in template
 // else: return placeholder data
 setInterval(function() {
-    Device.find().sort({ _id: -1 }).limit(10).then((query) => {
+    Device.find().sort({ _id: -1 }).limit(5).then((query) => {
         console.log(query);
         data = query;
     });
@@ -86,25 +86,66 @@ app.get("/", function(req, res) {
     // res.sendFile(__dirname + "/index.html");
     
     if (!(data === null || data.length == 0)) {
-        
-        let newest_entry = data[0]; 
-        let deviceTemp = newest_entry.deviceTemp;
-        let deviceHumidty = newest_entry.deviceHumidity;
-        let deviceMessage = newest_entry.deviceMessage;
-        let deviceTime = newest_entry.deviceTime;
 
         res.render('index', {
-            temperature: deviceTemp,
-            humidity: deviceHumidty,
-            time: deviceTime,
-            message: deviceMessage,
+            id_0 : data[0]._id.toString(),    
+            temperature_0: data[0].deviceTemp,
+            humidity_0: data[0].deviceHumidity,
+            time_0: data[0].deviceTime,
+            message_0: data[0].deviceMessage,
+
+            id_1 : data[1]._id.toString(),
+            temperature_1: data[1].deviceTemp,
+            humidity_1: data[1].deviceHumidity,
+            time_1: data[1].deviceTime,
+            message_1: data[1].deviceMessage,
+
+            id_2 : data[2]._id.toString(),
+            temperature_2: data[2].deviceTemp,
+            humidity_2: data[2].deviceHumidity,
+            time_2: data[2].deviceTime,
+            message_2: data[2].deviceMessage,
+
+            id_3 : data[3]._id.toString(),
+            temperature_3: data[3].deviceTemp,
+            humidity_3: data[3].deviceHumidity,
+            time_3: data[3].deviceTime,
+            message_3: data[3].deviceMessage,
+
+            id_4 : data[4]._id.toString(),
+            temperature_4: data[4].deviceTemp,
+            humidity_4: data[4].deviceHumidity,
+            time_4: data[4].deviceTime,
+            message_4: data[4].deviceMessage,
         })
     } else {
         res.render('index', {
-            temperature: '-1',
-            humidity: '-1',
-            time: '-1',
-            message: 'no data obtained from database',
+            id_0 : '-1',
+            temperature_0: '-1',
+            humidity_0: '-1',
+            time_0: '-1',
+            message_0: 'no data obtained from database',
+            id_1 : '-1',
+            temperature_1: '-1',
+            humidity_1: '-1',
+            time_1: '-1',
+            message_1: 'no data obtained from database',
+            id_2 : '-1',
+            temperature_2: '-1',
+            humidity_2: '-1',
+            time_2: '-1',
+            message_2: 'no data obtained from database',
+            id_3 : '-1',
+            temperature_3: '-1',
+            humidity_3: '-1',
+            time_3: '-1',
+            message_3: 'no data obtained from database',
+            id_4 : '-1',
+            temperature_4: '-1',
+            humidity_4: '-1',
+            time_4: '-1',
+            message_4: 'no data obtained from database',
+
         })
     }
     
